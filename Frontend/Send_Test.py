7d3d1cd62fd38ccb60ca410bbff8e6ee35567aa8
Tahmid490 <90710711+Tahmid490@users.noreply.github.com>
2022-02-19 17:38:58 -0500

    Add files via upload

---
+++ Frontend/Send_Test.py
@@ -0,0 +1,14 @@
+﻿#!/usr/bin/env python
+import pika
+
+credentials = pika.PlainCredentials(username='test', password='test')
+connection = pika.BlockingConnection(pika.ConnectionParameters('Our IP', credentials=credentials))
+channel = connection.channel()
+
+channel.queue_declare(queue='hello')
+
+channel.basic_publish(exchange='',
+                      routing_key='hello',
+                      body='Hello from Tahmid!')
+print(" [x] Sent 'Hello World!'")
+
