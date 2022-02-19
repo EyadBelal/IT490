69507d8f51898b9ab7dc44676adb31b494b24c8b
aa2669 <48959400+aa2669@users.noreply.github.com>
2022-02-19 17:31:17 -0500

    Add files via upload

---
+++ Backend/RRMQ.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+import pika, sys, os
+
+# connection to ali's 
+def main():
+    credentials = pika.PlainCredentials(username='test', password='test')
+    connection = pika.BlockingConnection(pika.ConnectionParameters(host='Put my IP in here', credentials=credentials))
+    channel = connection.channel()
+
+#declare the queue
+    channel.queue_declare(queue='hello')
+
+    def callback(ch, method, properties, body):
+        print(" [x] Received %r" % body)
+
+#callback the function
+    channel.basic_consume(callback, queue='hello', no_ack=True)
+
+    print(' [*] Waiting for messages. To exit press CTRL+C')
+    channel.start_consuming()
+
+#invoke the main function
+if __name__ == '__main__':
+    try:
+        main()
+    except KeyboardInterrupt:
+        print('Interrupted')
+        try:
+            sys.exit(0)
+        except SystemExit:
+            os._exit(0)
---
+++ Backend/SRMQ.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+import pika
+
+#Ali's site credentials
+credentials = pika.PlainCredentials(username='test', password='test')
+connection = pika.BlockingConnection(pika.ConnectionParameters(host='MY IP', credentials=credentials))
+
+channel = connection.channel()
+
+channel.queue_declare(queue='hello')
+
+channel.basic_publish(exchange='',
+                  routing_key='hello',
+                  body='Hello from Ali!')
+print(" [x] Sent 'Hello from Ali!'")
+connection.close()
