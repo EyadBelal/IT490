36df44270f584c4a008f9b1296bf603141665d9d
js2555 <87099460+js2555@users.noreply.github.com>
2022-03-10 21:39:30 -0500

    Update SRMQ.py

--- Backend/SRMQ.py
+++ Backend/SRMQ.py
@@ -1,9 +1,8 @@
 #!/usr/bin/env python
 import pika
 
-#Ali's site credentials
 credentials = pika.PlainCredentials(username='test', password='test')
-connection = pika.BlockingConnection(pika.ConnectionParameters(host='172.29.212.142', credentials=credentials))
+connection = pika.BlockingConnection(pika.ConnectionParameters(host='172.29.148.243', credentials=credentials))
 
 channel = connection.channel()
 
@@ -12,5 +11,5 @@ channel.queue_declare(queue='hello')
 channel.basic_publish(exchange='',
                   routing_key='hello',
                   body='Hello from Ali!')
-print(" [x] Sent 'Hello from Ali!'")
+print(" Jasprit Sent 'Hello from Ali!'")
 connection.close()
