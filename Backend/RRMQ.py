0c72a8662e6aeb8fd119b5d098ca143bd4cb1878
js2555 <87099460+js2555@users.noreply.github.com>
2022-03-10 21:41:38 -0500

    Update RRMQ.py

--- Backend/RRMQ.py
+++ Backend/RRMQ.py
@@ -11,7 +11,7 @@ def main():
     channel.queue_declare(queue='hello')
 
     def callback(ch, method, properties, body):
-        print(" [x] Received %r" % body)
+        print(" Eyad Received %r" % body)
 
 #callback the function
     channel.basic_consume(callback, queue='hello', no_ack=True)
