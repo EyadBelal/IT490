3acb4085a62bdea2b04c03345a1422b3b759a3cf
js2555 <87099460+js2555@users.noreply.github.com>
2022-05-10 22:17:20 +0000

    Add files via upload

---
+++ Milestone 4 & Midterm/Database/dbStar.php
@@ -0,0 +1,12 @@
+<?php
+ 
+ 
+$servername = "localhost";//Server Name
+$username= "root";//Server User Name
+$password = "Jasprit01!";//Server Password
+$dbname = "JATE";//Database Name
+ 
+//Create DB Connection
+$conn = mysqli_connect($servername,$username,$password,$dbname);
+ 
+?>
