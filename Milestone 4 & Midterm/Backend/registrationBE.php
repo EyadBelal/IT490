979e7bfd7106d8eb4c948a56f918c5eefb0712a5
js2555 <87099460+js2555@users.noreply.github.com>
2022-05-10 22:14:25 +0000

    Add files via upload

---
+++ Milestone 4 & Midterm/Backend/registrationBE.php
@@ -0,0 +1,40 @@
+#!/usr/bin/php
+<?php
+require_once('../RabbitMq/path.inc');
+require_once('../RabbitMq/get_host_info.inc');
+require_once('../RabbitMq/rabbitMQLib.inc');
+
+function queryDatabase($request)
+{
+	echo "Connecting to Database\n";
+	$client = new rabbitMQClient("../RabbitMq/registerToDB.ini", "testServer");
+	$validate = $request;
+	echo "Waiting for database to respond\n";
+	$response = $client->send_request($validate);
+	echo "Registered Successfully\n" . var_dump($response);
+	return $response;
+}
+
+function requestProcessor($request)
+{
+  echo "Connected".PHP_EOL;
+  var_dump($request);
+  if(!isset($request['username']))
+  {
+    return "ERROR: No username provided";
+  }
+  switch ($request['type'])
+  {
+		case "register":
+			return queryDatabase($request);
+	}
+	echo "Failed";
+	return "couldn't get message";
+}
+
+$server = new rabbitMQServer("../RabbitMq/register.ini","testServer");
+echo "Waiting for connection" . PHP_EOL;
+$server->process_requests('requestProcessor');
+exit();
+?>
+
